2c8c63445d7946157efcf37789b01614cfaedd95
Jan Kuris <jan.kuris@gmail.com>
2024-02-13 08:50:25 +0100

    end

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3' '4', '5', '6', '7', ' 8', '9']
+symbols = ["!", '@', "#", "$", "%", "_", " ", ")", "(", "*"]
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(plain_text, shift_amount):
-   cipher_text = ""
-   for letter in plain_text: 
-      access_list = alphabet.index(letter)
-      cipher_letter = access_list + shift_amount
-      if access_list >= 17:
-         # cipher_letter = access_list + shift_amount
-         if cipher_letter >= 25: 
-            cipher_letter = (24 - access_list) + shift_amount
-      new_letter = alphabet[cipher_letter]
-      cipher_text += new_letter
-   print(cipher_text)
+def ceasar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        access_to_list = alphabet.index(letter)
+        new_position = access_to_list + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction} text is {end_text}")
 
-
-print(encrypt(plain_text = text, shift_amount = shift))
-
-
-
+if shift >= 25:
+    shift = shift % 25
+   
+    
+ceasar(start_text = text, shift_amount = shift, cipher_direction = direction) 
 
 
