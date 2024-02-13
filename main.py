106d762184e6ec6ba252fb7eefac3b3952ec5892
Jan Kuris <jan.kuris@gmail.com>
2024-02-13 22:39:53 +0100

    repeat prg

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 numbers = ['0', '1', '2', '3' '4', '5', '6', '7', ' 8', '9']
 symbols = ["!", '@', "#", "$", "%", "_", " ", ")", "(", "*"]
 
+list_of_symbols = [alphabet, numbers, symbols]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+ceasar_end = False
 
 
 def ceasar(start_text, shift_amount, cipher_direction):
@@ -21,7 +23,18 @@ def ceasar(start_text, shift_amount, cipher_direction):
 if shift >= 25:
     shift = shift % 25
    
+
+
+while not ceasar_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    ceasar(start_text = text, shift_amount = shift, cipher_direction = direction)
     
-ceasar(start_text = text, shift_amount = shift, cipher_direction = direction) 
+    user_continue = input("If you want start again type 'yes'.\n").lower()
+    if user_continue == "yes":
+        ceasar(start_text = text, shift_amount = shift, cipher_direction = direction) 
+    else:
+        break 
 
 
